7705cad2e507ba4d57f8e4d5abdfde48fc9046bf
harmuthu <harmuthu@DIN66009063.corp.capgemini.com>
2018-07-06 17:32:08 +0530

    This is the Dummy code

---
+++ dummy/src/com/capg/Demo.java
@@ -0,0 +1,14 @@
+package com.capg;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hai");
+		System.out.println("hai");
+		System.out.println("hai");
+		System.out.println("hai");
+		System.out.println("hai");
+	}
+
+}
